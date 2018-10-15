97057696b4895636639ba3ba4c5596a7a80ae5f6
Maya <35238760+mayahilbert@users.noreply.github.com>
2018-10-14 20:36:20 -0400

    Initial commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ChangeScene : MonoBehaviour {
+    public string scenename;
+    public int seconds;
+
+    void OnTriggerEnter (Collider ChangeScene) {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Wait());
+            SceneManager.LoadScene(scenename);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        print(Time.time);
+        yield return new WaitForSecondsRealtime(seconds);
+        print(Time.time);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;            // The speed that the player will move at.
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Animator anim;                      // Reference to the animator component.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    public bool onGround;               // Check if player is on the ground.
+
+
+    void Awake()
+    {
+        // Set up references.
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        onGround = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        // Store the input axes.
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move the player around the scene.
+        Move(h, v);
+
+        // Turn the player to face the mouse cursor.
+        // Turn the player to face the direction of keyboard key pressed.
+        Turning();
+
+        // Animate the player.
+        Animating(h, v);
+
+        //Allow player to jump with spacebar.
+        Jumping();
+
+    }
+
+    void Move(float h, float v)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(h, 0f, v);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+    
+    void Turning()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        if (movement != Vector3.zero)
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
+    }
+
+    void Animating(float h, float v)
+    {
+        // Create a boolean that is true if either of the input axes is non-zero.
+        bool walking = h != 0f || v != 0f;
+
+        // Tell the animator whether or not the player is walking.
+        anim.SetBool("IsWalking", walking);
+    }
+
+    void Jumping()
+    {
+        if(onGround && Input.GetButtonDown("Jump"))
+        {
+            playerRigidbody.velocity = new Vector3(0f, 7f, 0f);
+            onGround = false;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision any)
+    {
+        any.gameObject.CompareTag("Ground");
+        onGround = true;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
